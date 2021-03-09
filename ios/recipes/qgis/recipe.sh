72317e00c274f0a76ea711afdb53f675c2daea2d
Denis Rouzaud <denis.rouzaud@gmail.com>
2021-03-09 09:35:35 +0100

    remove patch

--- ios/recipes/qgis/recipe.sh
+++ ios/recipes/qgis/recipe.sh
@@ -29,7 +29,6 @@ function prebuild_qgis() {
   then
     echo "\$O4iOS_qgis_DIR is not empty, manually patch your files if needed!"
   else
-    try patch -p1 < $RECIPE_qgis/patches/qgis.patch
     try patch -p1 < $RECIPE_qgis/patches/qgis-printer.patch
   fi
 
