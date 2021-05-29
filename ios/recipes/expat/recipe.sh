19c070465e593344cabac679c42a93cd9c2f4941
Matthias Kuhn <matthias@opengis.ch>
2021-05-29 09:30:52 +0200

    Bump expat version to 2.4.1

--- ios/recipes/expat/recipe.sh
+++ ios/recipes/expat/recipe.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # version of your package
-VERSION_expat=2.0.1
+VERSION_expat=2.4.1
 
 # dependencies of this recipe
 DEPS_expat=()
 
 # url of the package
-URL_expat=http://freefr.dl.sourceforge.net/project/expat/expat/$VERSION_expat/expat-${VERSION_expat}.tar.gz
+URL_expat=https://github.com/libexpat/libexpat/releases/download/R_$(echo $VERSION_expat | sed -r 's/\./_/g')/expat-$VERSION_expat.tar.gz
 
 # md5 of the package
-MD5_expat=ee8b492592568805593f81f8cdf2a04c
+MD5_expat=8738ef9ed1c5dcc03207286ea84f4299
 
 # default build path
 BUILD_expat=$BUILD_PATH/expat/$(get_directory $URL_expat)
