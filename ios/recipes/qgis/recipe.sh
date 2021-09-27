4d3845b83b6b85336a6d9e31c4d6d34275c2cb63
Denis Rouzaud <denis.rouzaud@gmail.com>
2021-09-27 13:56:56 +0200

    fix print support

--- ios/recipes/qgis/recipe.sh
+++ ios/recipes/qgis/recipe.sh
@@ -4,10 +4,10 @@
 DEPS_qgis=(protobuf libtasn1 gdal qca proj libspatialite libspatialindex expat postgresql libzip qtkeychain exiv2 qtlocation)
 
 # url of the package
-URL_qgis=https://github.com/qgis/QGIS/archive/6b80624e38016c96fd26a411515492170e8aa77d.tar.gz
+URL_qgis=https://github.com/3nids/QGIS/archive/e5f3a0fa38c24e0e26bc26ef25872626095e2fb1.tar.gz
 
 # md5 of the package
-MD5_qgis=352916af2c2cb8264dccd1d185b6230c
+MD5_qgis=93ca500a9fc55cc524a4658f4b690b0a
 
 # default build path
 BUILD_qgis=$BUILD_PATH/qgis/$(get_directory $URL_qgis)
