1031800d8f02798830dc86ec07fa92af88c59a3b
Denis Rouzaud <denis.rouzaud@gmail.com>
2022-05-13 15:05:14 +0200

    update libtasn1

--- ios/recipes/libtasn1/recipe.sh
+++ ios/recipes/libtasn1/recipe.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # version of your package
-VERSION_libtasn1=4.13
+VERSION_libtasn1=4.18
 
 # dependencies of this recipe
 DEPS_libtasn1=()
 
 # url of the package
-URL_libtasn1=https://gitlab.com/gnutls/libtasn1/-/archive/libtasn1_4_13/libtasn1-libtasn1_4_13.tar.gz
+URL_libtasn1=https://gitlab.com/gnutls/libtasn1/-/archive/v4.18.0/libtasn1-v4.18.0.tar.gz
 
 # md5 of the package
-MD5_libtasn1=349a320d12721227e29a9122bdaddea9
+MD5_libtasn1=1fcca15fd8a8d674383df74d4afed1f7
 
 # default build path
 BUILD_libtasn1=$BUILD_PATH/libtasn1/$(get_directory $URL_libtasn1)
